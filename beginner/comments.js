dabbb7b6283c221e5a923ae3e656e7fe4617aa56
caincoding <cain2311@gmail.com>
2019-04-25 16:57:20 -0500

    completed comments

---
+++ beginner/comments.js
@@ -0,0 +1,6 @@
+// double forward slashes are for single line comments
+
+/* use forward slash and astrick
+   to do a multi-line comment
+   and end the multi-line comment with an astrick and forward slash.
+*/
